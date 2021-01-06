c5b439dcd7cec035ea2e4a82c6dcac6b896f7947
jinglunwu-northsail <jing@northsail.io>
2021-01-06 16:36:59 -0500

    update the system spec test to break down to 3 contexts.

--- simple-task/spec/system/tasks_system_spec.rb
+++ simple-task/spec/system/tasks_system_spec.rb
@@ -1,83 +1,64 @@
 require "rails_helper"
 
 RSpec.describe "tasks management", type: :system do
-
-  before do
-    @user = create(:user)
-    sign_in(@user)
-  end
-
-  after do
-    sign_out(@user)
-  end
+  let(:user) { create(:user) }
 
   def create_a_task
     task = build(:task)
-    @task = @user.tasks.build({title: task.title, body: task.body, client: task.client, duration: task.duration, status: task.status, user_id: @user.id})
+    @task = user.tasks.build({title: task.title, body: task.body, client: task.client, duration: task.duration, status: task.status, user_id: user.id})
     @task.save
   end
 
-  it "user can add a new task" do
-    visit root_path
+  before do
+    sign_in(user)
+  end  
 
-    click_on "New Task"
+  after do
+    sign_out(user)
+  end 
 
-    @task = build(:task)
-    fill_in "task_title", with: @task.title
-    fill_in "task_body", with: @task.body
-    select @task.client
-    fill_in "task_duration", with: @task.duration
-    check "task_status"
+  context "create task" do
+    it "create a new task" do
+      visit root_path
+      click_on "New Task"
 
-    click_on "Create Task"
+      @task = build(:task)
+      fill_in "task_title", with: @task.title
+      fill_in "task_body", with: @task.body
+      select @task.client
+      fill_in "task_duration", with: @task.duration
+      check "task_status"
 
-    within('h1') do
-      expect(page).to have_content('Tasks List')
-    end
+      click_on "Create Task"
 
-    expect(Task.all.first.title).to eq(@task.title)
-    expect(Task.all.first.client).to eq(@task.client)
-    expect(Task.all.first.status).to eq(@task.status)
+      expect(page).to have_content(@task.title)
+    end 
   end
 
-  it "user can edit an existing task" do
-    create_a_task
+  context "edit task" do
+    it "edit an existing task" do
+      create_a_task
+      visit root_path
+      click_on "Edit"
 
-    visit root_path
-
-    click_on "Edit"
-
-
-    fill_in "task_title", with: @task.title + '!'
-    select "Molson"
-    uncheck "task_status"
-    click_on "Update Task"
-
-
-    expect(Task.count).to eq(1)
-    expect(Task.all.first.title).to eq(@task.title + '!')
-    expect(Task.all.first.client).to eq("Molson")
-    expect(Task.all.first.status).to eq("pending")
+      fill_in "task_title", with: @task.title + '!'
+      click_on "Update Task"
+      
+      expect(page).to have_content(@task.title + '!')     
+    end  
   end
 
-  it "user can delete an existing task" do
-    create_a_task
+  context "delete task" do
+    it "delete an existing task" do
+      create_a_task
+      visit root_path
 
-    visit root_path
+      accept_confirm do
+        click_on "Delete"
+      end
 
-    within('h1') do
-      expect(page).to have_content('Tasks List')
-    end
-
-    accept_confirm do
-      click_on "Delete"
-    end
-
-    within('h1') do
-      expect(page).to have_content('Tasks List')
-    end
-
-    expect(Task.count).to eq(0)
+      expect(page).to have_content('There is no tasks.')
+    end  
   end
-
+  
 end
