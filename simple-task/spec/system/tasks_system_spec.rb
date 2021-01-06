6e6d7f2f07286c8931bc29ce297640836ce609a0
jinglunwu-northsail <jing@northsail.io>
2021-01-06 17:22:45 -0500

    improve the system spec.

--- simple-task/spec/system/tasks_system_spec.rb
+++ simple-task/spec/system/tasks_system_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "tasks management", type: :system do
   let(:user) { create(:user) }
-  
+
   before do
     sign_in(user)
   end  
@@ -12,19 +12,20 @@ RSpec.describe "tasks management", type: :system do
   end 
 
   context "create task" do
-    it "create a new task" do
+    let(:task) { build(:task, user: user) }
+
+    before do 
       visit root_path
       click_on "New Task"
-
-      task = build(:task)
       fill_in "task_title", with: task.title
       fill_in "task_body", with: task.body
       select task.client
       fill_in "task_duration", with: task.duration
       check "task_status"
-
       click_on "Create Task"
+    end  
 
+    it "create a new task" do
       expect(page).to have_content(task.title)
     end 
   end
@@ -32,13 +33,14 @@ RSpec.describe "tasks management", type: :system do
   context "edit task" do
     let!(:task) { create(:task, user: user) }
 
-    it "edit an existing task" do
+    before do 
       visit root_path
-      click_on "Edit"
-
+      click_on "Edit"   
       fill_in "task_title", with: task.title + '!'
       click_on "Update Task"
-      
+    end  
+
+    it "edit an existing task" do       
       expect(page).to have_content(task.title + '!')     
     end  
   end
@@ -46,13 +48,15 @@ RSpec.describe "tasks management", type: :system do
   context "delete task" do
     let!(:task) { create(:task, user: user) }
 
-    it "delete an existing task" do
+    before do 
       visit root_path
 
       accept_confirm do
         click_on "Delete"
       end
+    end
 
+    it "delete an existing task" do
       expect(page).to have_content('There is no tasks.')
     end  
   end
