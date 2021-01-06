454301b66866f31c84b1bc5272a8d6255fc1a2cd
jinglunwu-northsail <jing@northsail.io>
2021-01-06 01:38:53 -0500

    Create a System Spec with rspec-rails, devise and factory_bot_rails

---
+++ simple-task/spec/factories.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+
+  factory :user do
+    email {"jing@northsail.io"}
+    password {"wjl113"}
+  end
+
+  factory :task do
+    title {"Ruby on Rails 6.0 Challenge"}
+    body {"Build a task demo using Rails Ruby on Rails 6.0. And check how to use devise for the authentication."}
+    client {"NorthSail"}
+    duration {20}
+    status {1}
+  end
+
+end
--- simple-task/spec/system/tasks_system_spec.rb
+++ simple-task/spec/system/tasks_system_spec.rb
@@ -1,15 +1,96 @@
 require "rails_helper"
 
 RSpec.describe "tasks management", type: :system do
-  include Devise::TestHelpers
 
   before do
-    user = {email: "jing@northsail.io", password: "wjl113"}
-    @user = User.Create!(user)
+    @user = create(:user)
     sign_in(@user)
   end
 
-  it "some test" do
-
+  after do
+    sign_out(@user)
   end
+
+  def create_a_task
+    task = build(:task)
+    @task = @user.tasks.build({title: task.title, body: task.body, client: task.client, duration: task.duration, status: task.status, user_id: @user.id})
+    @task.save
+  end
+
+  it "user can add a new task" do
+    visit root_path
+
+    within('h1') do
+      expect(page).to have_content('Tasks List')
+    end
+
+    click_on "New Task"
+
+    within('h1') do
+      expect(page).to have_content('Create New Task')
+    end
+
+    @task = build(:task)
+    fill_in "task_title", with: @task.title
+    fill_in "task_body", with: @task.body
+    select @task.client
+    fill_in "task_duration", with: @task.duration
+    check "task_status"
+
+    click_on "Create Task"
+
+    within('h1') do
+      expect(page).to have_content('Tasks List')
+    end
+
+    expect(Task.count).to eq(1)
+    expect(Task.all.first.title).to eq(@task.title)
+  end
+
+  it "user can edit an existing task" do
+    create_a_task
+
+    visit root_path
+
+    within('h1') do
+      expect(page).to have_content('Tasks List')
+    end
+
+    click_on "Edit"
+
+    within('h1') do
+      expect(page).to have_content('Update the Task')
+    end
+
+    fill_in "task_title", with: @task.title + '!'
+    click_on "Update Task"
+
+    within('h1') do
+      expect(page).to have_content('Tasks List')
+    end
+
+    expect(Task.count).to eq(1)
+    expect(Task.all.first.title).to eq(@task.title + '!')
+  end
+
+  it "user can delete an existing task" do
+    create_a_task
+
+    visit root_path
+
+    within('h1') do
+      expect(page).to have_content('Tasks List')
+    end
+
+    accept_confirm do
+      click_on "Delete"
+    end
+
+    within('h1') do
+      expect(page).to have_content('Tasks List')
+    end
+
+    expect(Task.count).to eq(0)
+  end
+
 end
